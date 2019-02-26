dacbb5b05af0b50c24fd155609fa4260e3b183de
Julie Mathews <juliem.forever@gmail.com>
2019-02-25 22:29:21 -0800

    finishing for homework

--- liri.js
+++ liri.js
@@ -1,73 +1,112 @@
-var keys = require('./keys.js');
+var keys = require("./keys.js");
 
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-
+var fs = require('fs');
 var client = new Twitter(keys.twitterKeys);
+var input = process.argv;
+var action = input[2];
+var inputs = input[3];
 
+switch (action) {
+	case "my-tweets":
+	twitter(inputs);
+	break;
 
-var getMyTweets = function() { 
-  var params = {screen_name: 'JulieMa93680133'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-      for(var i=0; i=tweets.length; i++) {
-        console.log(tweets[i].created_at);
-        console.log(' ');
-        console.log(tweets[i].text);
-      }
-    }
-  });
+	case "spotify-this-song":
+	spotify(inputs);
+	break;
+
+	case "movie-this":
+	movie(inputs);
+	break;
+
+	case "do-what-it-says":
+	doit();
+	break;
+};
+
+function twitter(inputs) {
+	var params = {screen_name: inputs, count: 20};
+	
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				for (i = 0; i < tweets.length; i ++){
+					console.log("Tweet: " + "'" + tweets[i].text + "'" + " Created At: " + tweets[i].created_at);
+				}
+			} else {
+				console.log(error);
+			}
+		});
+
 }
 
-var getArtistNames = function(artist) {
-  return artist.name;
+function spotify(inputs) {
+
+	var spotify = new Spotify(keys.spotifyKeys);
+		if (!inputs){
+        	inputs = 'The Sign';
+    	}
+		spotify.search({ type: 'track', query: inputs }, function(err, data) {
+			if (err){
+	            console.log('Error occurred: ' + err);
+	            return;
+	        }
+
+	        var songInfo = data.tracks.items;
+	        console.log("Artist(s): " + songInfo[0].artists[0].name);
+	        console.log("Song Name: " + songInfo[0].name);
+	        console.log("Preview Link: " + songInfo[0].preview_url);
+	        console.log("Album: " + songInfo[0].album.name);
+	});
 }
 
-var getMeSpotify = function(songName) {
-  spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      console.log('Error occurred: ' + err);
-      return;
-    }
-    var songs = data.tracks.items;
-    for (var i=0; i=songs.length; i++) {
-      console.log(i);
-      console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
-      console.log('song name: ' + songs[i].name);
-      console.log('preview song: ' + songs[i].preview_url);
-      console.log('album: ' + songs[i].album.name);
-      console.log('--------------------');
-    }
-    //console.log(data.tracks.items[0]); 
-  });
-}
 
 function movie(inputs) {
-  var queryURL = 'http://www.omdbapi.com/?t=' + inputs + "&y=&plot=short&apikey=47ed448";
-  request(queryURL, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-  });
 
-  var pick = function(caseData, functionData) {
-    switch(caseData) {
-      case 'my-tweets' :
-        getMyTweets();
-        break;
-      case 'spotify-this-song':
-        getMeSpotify(functionData);
-        break;
-      default:
-      console.log('LIRI does not know that');
-    }
-  }
+	var queryUrl = "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=40e9cece";
 
-  var runThis = function(argOne, argTwo) {
-    pick(argOne, argTwo);
-  };
+	request(queryUrl, function(error, response, body) {
+		if (!inputs){
+        	inputs = 'Mr Nobody';
+    	}
+		if (!error && response.statusCode === 200) {
 
-  runThis(process.argv[2], process.argv[3])
+		    console.log("Title: " + JSON.parse(body).Title);
+		    console.log("Release Year: " + JSON.parse(body).Year);
+		    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+		    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+		    console.log("Country: " + JSON.parse(body).Country);
+		    console.log("Language: " + JSON.parse(body).Language);
+		    console.log("Plot: " + JSON.parse(body).Plot);
+		    console.log("Actors: " + JSON.parse(body).Actors);
+		}
+	});
+};
+
+function doit() {
+	fs.readFile('random.txt', "utf8", function(error, data){
+
+		if (error) {
+    		return console.log(error);
+  		}
+
+		// Then split it by commas (to make it more readable)
+		var dataArr = data.split(",");
+
+		// Each command is represented. Because of the format in the txt file, remove the quotes to run these commands. 
+		if (dataArr[0] === "spotify-this-song") {
+			var songcheck = dataArr[1].slice(1, -1);
+			spotify(songcheck);
+		} else if (dataArr[0] === "my-tweets") {
+			var tweetname = dataArr[1].slice(1, -1);
+			twitter(tweetname);
+		} else if(dataArr[0] === "movie-this") {
+			var movie_name = dataArr[1].slice(1, -1);
+			movie(movie_name);
+		} 
+		
+  	});
+
 };
